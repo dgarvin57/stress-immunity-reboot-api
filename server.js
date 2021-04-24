d63827f8e49776cb4169492a2b62620eda40eba1
Dan Garvin <dgarvin57@gmail.com>
2021-04-23 20:43:16 -0500

    v0.1 - changes for heroku

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ const config = require("./config")
 // 04/23/21: v0.0 - Initial commit
 // *********************************************
 
-// Start server
-app.listen(3000, () =>
-  console.log(`stress-immunity-reboot-api listening on port 3000`)
-)
-
 // Default route
-app.get("/", (req, res) => res.send("Hello Dan2!"))
+app.get("/", (req, res) => res.send(`Hello Dan3! at ${currDate}`))
+
+// Start server
+const currDate = new Date()
+app.listen(process.env.PORT || 3000, () =>
+  console.log(
+    `stress-immunity-reboot-api listening on port ${process.env.PORT} || 3000 at ${currDate}`
+  )
+)
