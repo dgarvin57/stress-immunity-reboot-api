7777ef4c0279b1de3e84279a7810a4b916164e3d
Dan Garvin <dgarvin57@gmail.com>
2021-04-23 21:30:56 -0500

    v0.1 - bodyparser setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 const config = require("./config")
 
 // v0.0 - Initial setup
@@ -8,11 +9,30 @@ const config = require("./config")
 // 04/23/21: v0.0 - Initial commit
 // *********************************************
 
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
 const currDate = new Date()
 const port = process.env.PORT !== undefined ? process.env.PORT : 3000
 
 // Default route
-app.get("/", (req, res) =>
+app.get(
+  "/",
+  (req, res) =>
+    res.json({
+      status: "ok",
+      port: port,
+      message: "Hi Dan G",
+    })
+  //res.send(`Hello Dan3! at ${currDate} on port ${port}`)
+)
+
+// Database test route
+app.get("/dbtest", (req, res) =>
   res.send(`Hello Dan3! at ${currDate} on port ${port}`)
 )
 
